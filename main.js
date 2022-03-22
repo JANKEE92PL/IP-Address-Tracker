870e245b5d6b01a680f78c15ca3c9895eec42478
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-03-22 12:01:07 +0100

    handle api call and render it in the view

--- main.js
+++ main.js
@@ -12,10 +12,24 @@ const ApiCall = (ip = "8.8.8.8") => {
     `https://geo.ipify.org/api/v2/country?apiKey=at_Babs3OqgxR4kGQ90CV4zDL2l000zs&ipAddress=${ip}`
   )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => showInput(data));
+  //   .then((data) => console.log(data));
 };
 
 const searchBTN = getElement("#searchBTN");
 searchBTN.addEventListener("click", (e) => {
-  ApiCall(getInput());
+  let input = getInput();
+  ApiCall(input);
 });
+
+const ipAddress = getElement("#ip");
+const city = getElement("#city");
+const timezone = getElement("#timezone");
+const isp = getElement("#isp");
+
+const showInput = (data) => {
+  ipAddress.innerText = data.ip;
+  city.innerText = data.location.region;
+  timezone.innerText = data.location.timezone;
+  isp.innerText = data.isp;
+};
